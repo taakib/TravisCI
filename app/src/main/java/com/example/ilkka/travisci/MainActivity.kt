5007228fb604fd1513ecca9c701d5dc103672d57
taakib <piratoz22@gmail.com>
2018-10-25 12:31:36 +0300

    Third commit

---
+++ app/src/main/java/com/example/ilkka/travisci/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.ilkka.travisci
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun jee() : Boolean {
+        return false
+    }
+}
